ec48f650865c70679ce1e5a7ab3167fbf4602c3a
Diba Hadji Kazemi <98983201+dibahk@users.noreply.github.com>
2024-03-09 22:35:27 +0000

    Update Solution.py

--- 22540 Minimum Common Value/Solution.py
+++ 22540 Minimum Common Value/Solution.py
@@ -5,4 +5,15 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: int
         """
-        
+        i = 0 #the indicator of num1
+        j = 0 #indicator of num2
+        l1 = len(nums1)
+        l2 = len(nums2)
+        while i < l1 and j < l2:
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return -1
