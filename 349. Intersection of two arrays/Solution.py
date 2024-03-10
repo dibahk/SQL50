a765547eb89cc03f559fba951087594e0f9aad3f
Diba Hadji Kazemi <98983201+dibahk@users.noreply.github.com>
2024-03-10 09:57:03 +0000

    Update Solution.py

--- 349. Intersection of two arrays/Solution.py
+++ 349. Intersection of two arrays/Solution.py
@@ -6,3 +6,19 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        i = 0 #the indicator of num1
+        j = 0 #indicator of num2
+        l1 = len(nums1)
+        l2 = len(nums2)
+
+        dic = {}
+        while i < l1 and j < l2:
+            if nums1[i] == nums2[j]:
+                dic[nums1[i]] = 1
+                i += 1 
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return dic
