2c8c7c870e9dbd02a927a524de573c193df9af91
Diba Hadji Kazemi <98983201+dibahk@users.noreply.github.com>
2024-03-25 22:34:12 +0000

    Update Solution.py
    The previous version gave timeout error

--- 442. Find All Duplicates in an Array/Solution.py
+++ 442. Find All Duplicates in an Array/Solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        num = []
+        num = [0] * (len(nums)+1)
+
         res = []
         for i in nums:
-            if i in num:
+            if num[i] == 1:
                 res.append(i)
             else:
-                num.append(i)
+                num[i] = 1
         return res
