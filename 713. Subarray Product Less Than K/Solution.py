eb9024442e15eeb97a5f14bf29924d6217fb0343
Diba Hadji Kazemi <98983201+dibahk@users.noreply.github.com>
2024-03-27 12:44:13 +0000

    Update Solution.py

--- 713. Subarray Product Less Than K/Solution.py
+++ 713. Subarray Product Less Than K/Solution.py
@@ -1,3 +1,21 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        
+        # nums = list(set(nums))
+        nums.sort()        
+        count = 0
+        observed = []
+        n = len(nums)
+
+        for i in range(n):
+            pro = nums[i]
+            j = i
+            if nums[i] in observed:
+                count -= 1
+            observed.append(nums[i])
+            while pro < k and j < n:
+                pro = pro * nums[j]
+                j += 1
+                count += 1
+
+        return count
+
