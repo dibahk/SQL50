f6d00fbc31dafce069abada9645feeab8749175c
Diba Hadji Kazemi <98983201+dibahk@users.noreply.github.com>
2024-11-21 11:33:32 +0000

    Create 169. Majority Element.py

---
+++ 169. Majority Element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        n = len(nums)
+        for i in nums:
+            if i in count:
+                count[i] += 1
+                
+            else:
+               count[i] = 1
+               
+            if count[i] > n/2:
+                return i
